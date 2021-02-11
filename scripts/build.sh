a9bf52359e0f710feeb33f00b84f20881272b61a
Yash Totale <totaleyash@gmail.com>
2021-02-11 12:44:58 -0800

    Set up NPM + TypeScript configuration

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf dist
+ncc build src/index.ts --out dist
+mv dist/index.js dist/logger.js
+
+ncc build src/index.ts -m --out dist
+mv dist/index.js dist/logger.min.js
---
+++ scripts/postbuild.ts
@@ -0,0 +1,33 @@
+// Node
+import { join } from "path";
+import { writeFile, rm } from "fs/promises";
+
+// Externals
+import recursive from "recursive-readdir";
+
+const keepTypings = ["dist/index.d.ts"];
+
+const postbuild = async () => {
+  try {
+    writeFile(
+      join("dist", "index.js"),
+      "'use strict';\n\nif (process.env.NODE_ENV === 'production') {\n  module.exports = require('./logger.min.js');" +
+        "\n} else {\n  module.exports = require('./logger.js');\n}"
+    );
+    recursive("dist").then((files) => {
+      files.forEach((file) => {
+        if (file.length > 5) {
+          const ending = file.substr(file.length - 5);
+          if (ending === ".d.ts" && !keepTypings.includes(file)) {
+            rm(file);
+          }
+        }
+      });
+    });
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+};
+
+postbuild();
