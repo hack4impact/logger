7917c6a19e1fd44078c05e77c057fa0720101b91
Yash Totale <totaleyash@gmail.com>
2021-02-12 19:59:18 -0800

    Added Logger.line Tests

---
+++ tests/static-methods/line.test.ts
@@ -0,0 +1,12 @@
+// Internals
+import Logger from "../../src";
+import { setUpConsoleSpy } from "../helpers";
+
+test("Prints empty line", () => {
+  const spy = setUpConsoleSpy();
+
+  Logger.line();
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenLastCalledWith<[]>();
+});
